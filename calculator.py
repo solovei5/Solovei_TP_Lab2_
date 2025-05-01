711bb3cd5115604a0ab1ac3ba372845d59ca90b7
solovei5 <vlad2005solovei@gmail.com>
2025-05-01 15:28:58 +0300

    Add basic calculator functions (add, subtract).

---
+++ calculator.py
@@ -0,0 +1,11 @@
+# calculator.py
+def add(x, y):
+    return x + y
+def subtract(x, y):
+    return x - y
+if __name__ == "__main__":
+    num1 = 15
+    num2 = 7
+    print(f"{num1} + {num2} = {add(num1, num2)}")
+    print(f"{num1} - {num2} = {subtract(num1, num2)}")
+print("Базовий калькулятор готовий.") # Цей рядок будемо змінювати
