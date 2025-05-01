40fa847ba77fb17bbd237fa64c9644d884032928
solovei5 <vlad2005solovei@gmail.com>
2025-05-01 15:37:14 +0300

    Add basic calculator functions (add, subtract). feat: Add multiply function and usage fix: Update completion message in main.

--- calculator.py
+++ calculator.py
@@ -3,9 +3,11 @@ def add(x, y):
     return x + y
 def subtract(x, y):
     return x - y
+
 if __name__ == "__main__":
     num1 = 15
     num2 = 7
     print(f"{num1} + {num2} = {add(num1, num2)}")
     print(f"{num1} - {num2} = {subtract(num1, num2)}")
-print("Базовий калькулятор готовий.") # Цей рядок будемо змінювати
+# Тут немає виклику multiply!
+print("Виправлено текст повідомлення у main.") # Змінений той самийрядок!
